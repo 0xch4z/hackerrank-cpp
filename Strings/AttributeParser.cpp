e9071834e1e1271e617feb5c4df82f4589f5cb1d
Charles Kenney <Charlesc.kenney@gmail.com>
2017-07-21 03:37:31 -0400

    added more class & string solutions

---
+++ Strings/AttributeParser.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <regex>
+#include <vector>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::getline;
+using std::vector;
+
+enum Node {
+    tag,
+    label,
+    value
+}
+
+int main() {
+
+    // Get meta data
+    int N, Q;
+    cin >> N >> Q;
+
+    string line, lines;
+
+    getline(cin, line);
+
+    for (int i = 0; i < N; i++) {
+
+    }
+
+
+}
